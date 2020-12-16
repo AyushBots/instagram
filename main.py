cd3f8603d18fa4425b046e3837022c1300313ab2
AYU$H $HARMA <aayush240204@gmail.com>
2020-12-16 15:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1442240140:AAFCuGCsYcsrr3XzVL7R5cIAUCDSuwraxQ4"
 
 def download(bot, update):
     message = update.effective_message
